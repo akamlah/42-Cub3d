ac510f08d208edd371f9bf90defe48002d771636
Alice Kamlah <akamlah@1-C-8.42heilbronn.de>
2022-02-25 15:41:53 +0100

    more compact code and loop for increments in hor intersections

--- sources/minimap.c
+++ sources/minimap.c
@@ -76,95 +76,68 @@ void	draw_minimap(t_vars *vars)
 	draw_blocks_minimap(vars);
 	draw_player_minimap(vars);
 
+
 	vars->prjp = new_image(vars, PRJP_W, PRJP_H, 20, 20);
 
 	// find HORIZONTAL COLLISIONS:
-	double first_hor_grid_coll_RW_x = 0;
-	double first_hor_grid_coll_RW_y = 0;
 	double range_rounding = 0.0000001;
+	double dy = RW_UNIT;
+	double dx = RW_UNIT / tan(vars->player->th);
+	double first_hor_grid_coll_RW_dx = 0; // dx (player pos, 1st hor intersection)
+	double first_hor_grid_coll_RW_dy = 0; // dy (player pos, 1st hor intersection)
+	double hor_hit_RW_x = 0;
+	double hor_hit_RW_y = 0;
+	double hor_hit_player_dist_RW = 0;
+	int	y_increment_sign = 1;
 
-	// find coordinates of 1st intersection with the horizontal gridlines from player position.
-
-	// edge cases... excluded from drawing
-	// around 0
-	if (vars->player->th <= range_rounding || vars->player->th >= (M_PI * 2) - range_rounding)
+	// find coordinates of 1st intersection with the horizontal gridlines from player position:
+	// 0 and 180 no hor. intersections.
+	// 1st octant && 2nd octant
+	if (range_rounding < vars->player->th && M_PI - range_rounding > vars->player->th) // from 0+ to M_PI-
 	{
-		first_hor_grid_coll_RW_x = (vars->map->max_width - 1) * RW_UNIT;
-		first_hor_grid_coll_RW_y = vars->player->RW_y;
+		first_hor_grid_coll_RW_dx = (vars->player->RW_y % RW_UNIT) / tan(vars->player->th); // > 0 1st ioct, < 0 2nd oct
+		first_hor_grid_coll_RW_dy = - (vars->player->RW_y % RW_UNIT);
+		if (vars->player->th <= M_PI_2 + range_rounding && vars->player->th >= M_PI_2 - range_rounding) // 90 degs edge case for tan
+			first_hor_grid_coll_RW_dx = 0;
+		dy *= -1;
+		y_increment_sign *= -1;
 	}
-	// around 90
-	if (vars->player->th <= M_PI_2 + range_rounding && vars->player->th >= M_PI_2 - range_rounding)
+	// 3rd octant && 4th octant
+	if ((vars->player->th > M_PI + range_rounding && vars->player->th < (M_PI * 2) - range_rounding))
 	{
-		first_hor_grid_coll_RW_x = vars->player->RW_x;
-		first_hor_grid_coll_RW_y = (vars->player->RW_y / RW_UNIT) * RW_UNIT;
-	}
-	// around 180
-	if (vars->player->th >= M_PI - range_rounding && vars->player->th <= M_PI + range_rounding)
-	{
-		first_hor_grid_coll_RW_x = 0;
-		first_hor_grid_coll_RW_y = vars->player->RW_y;
-	}
-	// around 270
-	if (vars->player->th >= 3 * M_PI_2 - range_rounding && vars->player->th <= 3 * M_PI_2 + range_rounding)
-	{
-		first_hor_grid_coll_RW_x = vars->player->RW_x;
-		first_hor_grid_coll_RW_y = ((vars->player->RW_y / RW_UNIT) + 1) * RW_UNIT;
+		first_hor_grid_coll_RW_dx = - ((RW_UNIT - vars->player->RW_y % RW_UNIT) / tan(vars->player->th));
+		if (vars->player->th >= 3 * M_PI_2 - range_rounding && vars->player->th <= 3 * M_PI_2 + range_rounding) // 270 edge case
+			first_hor_grid_coll_RW_dx = 0;
+		first_hor_grid_coll_RW_dy = RW_UNIT - vars->player->RW_y % RW_UNIT;
+		dx *= -1;
 	}
 
-	// double dy = RW_UNIT;
-	// double dx = RW_UNIT / tan(vars->player->th);
-
-	// 1st octant
-	// 2nd octant
-	if ((range_rounding < vars->player->th && M_PI_2 -  range_rounding > vars->player->th)
-		|| (M_PI_2 + range_rounding < vars->player->th && M_PI - range_rounding > vars->player->th))
+	if (!(vars->player->th >= M_PI - range_rounding && vars->player->th <= M_PI + range_rounding) // 180
+		&& !(vars->player->th <= range_rounding || vars->player->th >= (M_PI * 2) - range_rounding)) //0
 	{
-		first_hor_grid_coll_RW_x = vars->player->RW_x + ((vars->player->RW_y % RW_UNIT) / tan(vars->player->th));
-		first_hor_grid_coll_RW_y = (vars->player->RW_y / RW_UNIT) * RW_UNIT;
-		// next hit has x coord of first hit + dx, y coord of first hit + dy;
-		/// if init i = 0, iterate in i * dx, i * dy orr something like that.make
-		if (!(first_hor_grid_coll_RW_x <= 0
-		|| first_hor_grid_coll_RW_y <= 0
-		|| first_hor_grid_coll_RW_x >= (vars->map->max_width - 1) * RW_UNIT
-		|| first_hor_grid_coll_RW_y >= vars->map->n_lines * RW_UNIT))
-		if (vars->map->nodes[((int)first_hor_grid_coll_RW_y - RW_UNIT / 2) / RW_UNIT][(int)first_hor_grid_coll_RW_x / RW_UNIT] == '1')
+		hor_hit_RW_x = vars->player->RW_x + first_hor_grid_coll_RW_dx;
+		hor_hit_RW_y = vars->player->RW_y + first_hor_grid_coll_RW_dy;
+		while (hor_hit_RW_y < vars->map->n_lines * RW_UNIT && hor_hit_RW_y > 0 && hor_hit_RW_x < (vars->map->max_width - 1) * RW_UNIT && hor_hit_RW_x > 0) // while lines in map basically
 		{
-			draw_square_tlc(vars->minimap->img, vars->minimap->scale, vars->minimap->scale, \
-			((int)first_hor_grid_coll_RW_x / RW_UNIT) * vars->minimap->scale, \
-			(((int)first_hor_grid_coll_RW_y - RW_UNIT / 2) / RW_UNIT) * vars->minimap->scale, 0x00ff00);
-		}
-	}
-
-	// 3rd octant
-	// 4th octant
-	if ((vars->player->th > M_PI + range_rounding && vars->player->th < 3 * M_PI_2 - range_rounding)
-		|| (vars->player->th > 3 * M_PI_2 + range_rounding && vars->player->th < (M_PI * 2) - range_rounding))
-	{
-		first_hor_grid_coll_RW_x = vars->player->RW_x - ((RW_UNIT - vars->player->RW_y % RW_UNIT) / tan(vars->player->th)); 
-		first_hor_grid_coll_RW_y = ((vars->player->RW_y / RW_UNIT) + 1) * RW_UNIT;
-		if (!(first_hor_grid_coll_RW_x <= 0
-			|| first_hor_grid_coll_RW_y <= 0
-			|| first_hor_grid_coll_RW_x >= (vars->map->max_width - 1) * RW_UNIT
-			|| first_hor_grid_coll_RW_y >= vars->map->n_lines * RW_UNIT))
-		if (vars->map->nodes[((int)first_hor_grid_coll_RW_y + RW_UNIT / 2) / RW_UNIT][(int)first_hor_grid_coll_RW_x / RW_UNIT] == '1')
-		{
-			draw_square_tlc(vars->minimap->img, vars->minimap->scale, vars->minimap->scale, \
-			((int)first_hor_grid_coll_RW_x / RW_UNIT) * vars->minimap->scale, \
-			(((int)first_hor_grid_coll_RW_y + RW_UNIT / 2) / RW_UNIT) * vars->minimap->scale, 0x00ff00);
-		}
-	}
-
-	if (!(first_hor_grid_coll_RW_x <= 0
-		|| first_hor_grid_coll_RW_y <= 0
-		|| first_hor_grid_coll_RW_x >= (vars->map->max_width - 1) * RW_UNIT
-		|| first_hor_grid_coll_RW_y >= vars->map->n_lines * RW_UNIT))
 			draw_line(vars->minimap->img, \
 				vars->player->RW_x * vars->minimap->scale / RW_UNIT, \
 				vars->player->RW_y * vars->minimap->scale / RW_UNIT, \
-				(int)first_hor_grid_coll_RW_x * vars->minimap->scale / RW_UNIT, \
-				(int)first_hor_grid_coll_RW_y * vars->minimap->scale / RW_UNIT);
-	// if statement makes sure that only if intersection exists  the line is drawwn -> use later to check.
-
+				(int)hor_hit_RW_x * vars->minimap->scale / RW_UNIT, \
+				(int)hor_hit_RW_y * vars->minimap->scale / RW_UNIT);
+			if (vars->map->nodes[((int)hor_hit_RW_y + y_increment_sign * (RW_UNIT / 2)) / RW_UNIT][(int)hor_hit_RW_x / RW_UNIT] == '1')
+			{
+				draw_square_tlc(vars->minimap->img, vars->minimap->scale, vars->minimap->scale, \
+				((int)hor_hit_RW_x / RW_UNIT) * vars->minimap->scale, \
+				(((int)hor_hit_RW_y + y_increment_sign * (RW_UNIT / 2)) / RW_UNIT) * vars->minimap->scale, 0x00ff00);
+				// find hit distance.
+				hor_hit_player_dist_RW = (vars->player->RW_y - hor_hit_RW_y) / sin(vars->player->th); // sinus: no need for 90 deg edge case.
+					printf("DIST: %f %d\n", hor_hit_player_dist_RW, (int)hor_hit_player_dist_RW);
+				break ;
+			}
+			hor_hit_RW_x += dx;
+			hor_hit_RW_y += dy;
+		}
+	}
 
 	// put to window
 	mlx_put_image_to_window(vars->mlx_vars->mlx_ptr, \
@@ -174,3 +147,20 @@ void	draw_minimap(t_vars *vars)
 		vars->minimap->img->S_ytlc);
 
 }
+
+	// double first_hor_grid_coll_RW_x = 0;
+	// double first_hor_grid_coll_RW_y = 0;
+	// edge cases... excluded from drawing
+	// around 0
+	// if (vars->player->th <= range_rounding || vars->player->th >= (M_PI * 2) - range_rounding)
+	// {
+	// 	first_hor_grid_coll_RW_x = (vars->map->max_width - 1) * RW_UNIT;
+	// 	first_hor_grid_coll_RW_y = vars->player->RW_y;
+	// }
+
+	// // around 180
+	// if (vars->player->th >= M_PI - range_rounding && vars->player->th <= M_PI + range_rounding)
+	// {
+	// 	first_hor_grid_coll_RW_x = 0;
+	// 	first_hor_grid_coll_RW_y = vars->player->RW_y;
+	// }
